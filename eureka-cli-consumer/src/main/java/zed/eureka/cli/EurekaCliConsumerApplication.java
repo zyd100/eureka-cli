81cd96ae862087901f39e7dd525f4c44d4358e4f
zedzheng <1019661738@qq.com>
2021-07-05 21:14:59 +0800

    init consumer

---
+++ eureka-cli-consumer/src/main/java/zed/eureka/cli/EurekaCliConsumerApplication.java
@@ -0,0 +1,13 @@
+package zed.eureka.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCliConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaCliConsumerApplication.class, args);
+    }
+
+}
