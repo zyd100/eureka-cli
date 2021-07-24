54e0b4ffaecc4733c14b1970224afaa5a5e3e3ed
zedzheng <1019661738@qq.com>
2021-07-24 20:38:30 +0800

    init gateway project

---
+++ eureka-cli-gateway/src/main/java/zed/eureka/cli/gateway/EurekaCliGatewayApplication.java
@@ -0,0 +1,13 @@
+package zed.eureka.cli.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaCliGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaCliGatewayApplication.class, args);
+    }
+
+}
---
+++ eureka-cli-gateway/src/test/java/zed/eureka/cli/gateway/EurekaCliGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package zed.eureka.cli.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaCliGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
